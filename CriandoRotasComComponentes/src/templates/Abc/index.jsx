7e963016d9b9c6307e7be2be1346476204bcd999
Rafael Dametto <cacotpds@gmail.com>
2023-08-15 10:30:56 -0300

    "Route takes all" - 404 Not Found

--- CriandoRotasComComponentes/src/templates/Abc/index.jsx
+++ CriandoRotasComComponentes/src/templates/Abc/index.jsx
@@ -1,21 +1,7 @@
-import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom';
-import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 
 export const Abc = () => {
   const { slug, id } = useParams();
-  const location = useLocation();
-  const history = useHistory();
-
-  console.log(location);
-  console.log(history);
-
-  useEffect(() => {
-    setTimeout(function () {
-      history.push('/');
-    }, 5000);
-  }, [history]);
 
   return (
     <div>
