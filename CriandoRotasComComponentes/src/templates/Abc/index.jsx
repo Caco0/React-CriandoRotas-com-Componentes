2ee36d80da4d8c8265193c32fd4c5bfa071fd089
Rafael Dametto <cacotpds@gmail.com>
2023-08-15 09:50:33 -0300

    useHistory and useLocation

--- CriandoRotasComComponentes/src/templates/Abc/index.jsx
+++ CriandoRotasComComponentes/src/templates/Abc/index.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 
 export const Abc = () => {
   const { slug, id } = useParams();
+  const location = useLocation();
+  const history = useHistory();
+
+  console.log(location);
+  console.log(history);
+
+  useEffect(() => {
+    setTimeout(function () {
+      history.push('/');
+    }, 5000);
+  }, [history]);
 
   return (
     <div>
